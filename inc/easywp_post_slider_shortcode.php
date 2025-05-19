e1e15346e58c6bf294ebe146965420183022f0ca
arifmanzoor11 <arifmanzoor11@gmail.com>
2025-05-19 21:19:51 +0530

    impove style of slick slider shortcode

--- inc/easywp_post_slider_shortcode.php
+++ inc/easywp_post_slider_shortcode.php
@@ -58,7 +58,7 @@ function easywp_post_slider_shortcode($atts) {
     align-items: center;
     justify-content: space-between;">
         <div>
-            <?php echo '<h2 class="easywp-slider-heading">' . esc_html($category->name) . '</h2>';
+            <?php echo '<h2 class="easywp-slider-heading ps-4"><b>' . esc_html($category->name) . '</b></h2>';
              ?>
         </div>
         <div>
@@ -68,11 +68,11 @@ function easywp_post_slider_shortcode($atts) {
     <div class="easywp-slick-posts ">
         <?php while ($query->have_posts()) : $query->the_post(); ?>
             <div class="post-slide p-2">
-                <div class="card h-100 shadow-sm">
+                <div class="card h-100 shadow-sm border">
                     <a href="<?php the_permalink(); ?>" class="slick-posts-anchor">
-                        <div class="card-img-top">
+                        <div class="card-img-top ps-3 pt-3">
                             <?php if (has_post_thumbnail()) : ?>
-                                <?php the_post_thumbnail('medium', ['class' => 'img-fluid', 'alt' => get_the_title()]); ?>
+                                <?php the_post_thumbnail('thumbnail', ['class' => 'img-fluid', 'alt' => get_the_title()]); ?>
                                 <?php else : ?>
                                 <?php
                                 // Generate random color in hex
@@ -93,21 +93,18 @@ function easywp_post_slider_shortcode($atts) {
                                 $rand_color = $modern_colors[array_rand($modern_colors)];
                                 ?>
                                 <!-- Fallback SVG with random color -->
-                                <svg class="img-fluid w-100" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="<?php the_title(); ?>">
+                                <svg class="img-fluid rounded" width="150px" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="<?php the_title(); ?>">
                                     <rect width="100%" height="100%" fill="<?php echo esc_attr($rand_color); ?>"/>
                                     <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#fff" font-size="30">
-                                    
                                     No Image
                                     </text>
-                                    
                                 </svg>
                             <?php endif; ?>
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title"><?php the_title(); ?></h5>
+                            <h6 class="card-title"><b><?php the_title(); ?></b></h6>
                             <p class="card-text"><?php echo esc_html(wp_trim_words(get_the_excerpt(), 10)); ?></p>
-                            <br>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-main btn-sm">Read More</a>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-main btn-sm mt-3">Read More</a>
                         </div>
                     </a>
                 </div>
